15f6535396f8896a01b9a1dc5ff29ca805524bf8
Alexis-j <alexis.jcastillo@outlook.com>
2023-11-03 14:11:10 +0100

    php require incluide

--- 17-require.php
+++ 17-require.php
@@ -1,8 +1,7 @@
 <?php include 'includes/header.php';
 
+require 'funciones.php';
 
-
-
-
+iniciarAPP();
 
 include 'includes/footer.php';
--- 18.json.php
+++ 18.json.php
@@ -1,5 +1,34 @@
 <?php include 'includes/header.php';
 
+$produtos = [
+    [
+        'nombre' => 'Tablet',
+        'precio' => 200,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Television',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor',
+        'precio' => 400,
+        'disponible' => false
+    ]
+];
+
+echo "<pre>";
+var_dump($produtos);
+
+$jason = json_encode($produtos);
+
+$json_array = json_decode($jason);
+
+var_dump($jason);
+echo "</pre>";
+var_dump($json_array);
+echo "</pre>";
 
 
 include 'includes/footer.php';
