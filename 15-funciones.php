664847ae8515aa151a4399b992842b81dd7435b8
Alexis-j <alexis.jcastillo@outlook.com>
2023-11-03 11:07:28 +0100

    php require incluide

--- 15-funciones.php
+++ 15-funciones.php
@@ -1,7 +1,13 @@
-<?php include 'includes/header.php';
+<?php 
+declare(strict_types=1); // Para que PHP sea estricto en el tipo de dato que se le pasa a la función	
+include 'includes/header.php';
 
 
+function Sumar(int $numero1 = 0, int $numero2 = 0) {
+    echo $numero1 + $numero2;
+    echo "<br>"; 
+}
 
-
+sumar(10, 30);
 
 include 'includes/footer.php';
--- 16-types.php
+++ 16-types.php
@@ -1,7 +1,14 @@
 <?php include 'includes/header.php';
 
+function usuarioAutenticado(bool $autenticado) : string {
+    if($autenticado) {
+        return "El usuario está autenticado";
+    } else {
+        return "El usuario no está autenticado";
+    }
+}
 
-
-
+$usuario = usuarioAutenticado(false);
+echo $usuario;
 
 include 'includes/footer.php';
